35ef2cd92c05409eca2a12a71c1fb8d805d7eb89
Miłosz <milsli69.gmail.com>
2024-12-27 12:40:53 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string naMasterze()
+{
+    return "\nJestem na masterze\n";
+}
+
+int main()
+{
+    cout << naMasterze();
+
+    return 0;
+}
